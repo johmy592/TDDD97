f83cf15530ae3f1b07496f9ba6e016ad4029fe2f
johmy592 <johmy592@su02-110.ad.liu.se>
2019-03-05 11:55:32 +0100

    Some refactoring

--- client.js
+++ client.js
@@ -1,40 +1,38 @@
-/* Chooses which page to show */
 displayView = function () {
     if (localStorage.getItem("token") === null) {
-        document.getElementById("view").innerHTML = document.getElementById("welcomeview").innerHTML;
+        document.getElementById("display_view").innerHTML = document.getElementById("welcomeview").innerHTML;
 
-        /* EventListeners */
         document.getElementById('login_form').addEventListener('submit', logIn);
         document.getElementById('signup_form').addEventListener('submit', registerUser);
         document.getElementById('signup_email').addEventListener('input', resetValidation);
 
-        //Validator password length and match.
         document.getElementById('signup_password').addEventListener('input',
             function () {
-                pwdLength('signup_password')
+                passwordLength('signup_password')
             });
         document.getElementById('repeat_password').addEventListener('input',
             function () {
-                pwdMatch('signup_password', 'repeat_password')
+                passwordMatch('signup_password', 'repeat_password')
             });
     } else {
-        //Opens profile view at start tab and loads profile data from server.
-        document.getElementById("view").innerHTML = document.getElementById("profileview").innerHTML;
+
+        document.getElementById("display_view").innerHTML = document.getElementById("profileview").innerHTML;
         getUserData();
         updateWall('message_wall');
-        /* EventListeners */
-        document.getElementById('home_tab_btn').addEventListener('click', function(ev){showPage(ev, 'home_tab')});
-        document.getElementById('browse_tab_btn').addEventListener('click', function(ev){showPage(ev, 'browse_tab')});
-        document.getElementById('account_tab_btn').addEventListener('click', function(ev){showPage(ev, 'account_tab')});
+
+
+        document.getElementById('home_tab_btn').addEventListener('click', function(ev){showTab(ev, 'home_tab')});
+        document.getElementById('browse_tab_btn').addEventListener('click', function(ev){showTab(ev, 'browse_tab')});
+        document.getElementById('account_tab_btn').addEventListener('click', function(ev){showTab(ev, 'account_tab')});
 
         document.getElementById('message_button').addEventListener('click',
             function () {
-                publishMessage('message_box', 'email_info', 'message_wall')
+                sendMessage('message_box', 'email_info', 'message_wall')
             });
-        document.getElementById("browse_user").addEventListener('submit', searchForUser);
+        document.getElementById("browse_user").addEventListener('submit', browseUser);
         document.getElementById('send_browse_msg').addEventListener('click',
             function () {
-                publishMessage('browse_message', 'user_email', 'browse_message_wall')
+                sendMessage('browse_message', 'user_email', 'browse_message_wall')
             });
         document.getElementById('reload_browse_wall_btn').addEventListener('click',
             function () {
@@ -44,19 +42,20 @@ displayView = function () {
             function () {
                 updateWall('message_wall')
             });
-        document.getElementById('password_change').addEventListener('submit', new_password);
-        //Validator password length and match.
+        document.getElementById('password_change').addEventListener('submit', changePassword);
+
+
         document.getElementById('new_password').addEventListener('input',
             function () {
-                pwdLength('new_password')
+                passwordLength('new_password')
             });
         document.getElementById('confirm_new_pw').addEventListener('input',
             function () {
-                pwdMatch('new_password', 'confirm_new_pw')
+                passwordMatch('new_password', 'confirm_new_pw')
             });
-        document.getElementById('sign_out').addEventListener('submit', sign_out);
+        document.getElementById('sign_out').addEventListener('submit', logOut);
 
-        // Opens start tab
+        //Explicitly call click() to show the home page
         document.getElementById("home_tab_btn").click();
     }
 
@@ -71,37 +70,36 @@ window.onload = function () {
     //window.alert() is not allowed to be used in your implementation.
 };
 
-/* Tab functionality */
-showPage = function (evt, tab) {
-    var i, content, tablinks;
-    //Hides all pages to begin with.
+/* Switching tabs */
+showTab = function (evt, tab) {
+    var i, content, tab_buttons;
+    //Make all tabs hidden
     content = document.getElementsByClassName('tab_content');
     for (i = 0; i < content.length; i++) {
         content[i].style.display = "none";
     }
 
-    //set all tabs inactive
-    tablinks = document.getElementsByClassName('tab_button');
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    //Make all tabs inactive
+    tab_buttons = document.getElementsByClassName('tab_button');
+    for (i = 0; i < tab_buttons.length; i++) {
+        tab_buttons[i].className = tab_buttons[i].className.replace(" active", "");
     }
-
+    // Show the specified tab
     document.getElementById(tab).style.display = "block";
     evt.currentTarget.className += " active";
 };
 
-/* Start panel */
 
-/* Retrieves profile information for start page */
+/* Retrieves information about user for home page */
 getUserData = function () {
     var token = localStorage.getItem('token');
-    var returnObject = serverstub.getUserDataByToken(token).data;
-    var name = returnObject.firstname;
-    var family_name = returnObject.familyname;
-    var email = returnObject.email;
-    var gender = returnObject.gender;
-    var city = returnObject.city;
-    var country = returnObject.country;
+    var serverResponse = serverstub.getUserDataByToken(token).data;
+    var name = serverResponse.firstname;
+    var family_name = serverResponse.familyname;
+    var email = serverResponse.email;
+    var gender = serverResponse.gender;
+    var city = serverResponse.city;
+    var country = serverResponse.country;
 
     document.getElementById('name_info').innerText = name + " " + family_name;
     document.getElementById('email_info').innerText = email;
@@ -112,53 +110,53 @@ getUserData = function () {
 };
 
 
-publishMessage = function (text_input, receiver_email, msg_wall) {
-    var message = document.getElementById(text_input).value;
+sendMessage = function (message_input, receiver_email, msg_wall) {
     var token = localStorage.getItem('token');
+    var message = document.getElementById(message_input).value;
     var email = document.getElementById(receiver_email).innerText;
-    console.log(message, token, email);
     serverstub.postMessage(token, message, email);
     updateWall(msg_wall);
 };
 
 updateWall = function (msg_wall) {
-    document.getElementById(msg_wall).innerHTML = "";
     var token = localStorage.getItem('token');
-    var messageArray;
-    if (msg_wall === 'browse_message_wall'){
-        var email = document.getElementById('user_email').innerText;
-        messageArray = serverstub.getUserMessagesByEmail(token, email).data
+    var messages;
+
+    if (msg_wall === 'message_wall'){
+      // Posting to your own wall
+      messages = serverstub.getUserMessagesByToken(token).data;
     }else {
-        messageArray = serverstub.getUserMessagesByToken(token).data;
+        // Posting to someone elses wall
+        var email = document.getElementById('user_email').innerText;
+        messages = serverstub.getUserMessagesByEmail(token, email).data;
     }
-    console.log(messageArray);
+    console.log(messages);
+    document.getElementById(msg_wall).innerHTML = "";
     var i;
-    for (i = 0; i < messageArray.length; i++) {
-        var writer = messageArray[i].writer;
-        var content = messageArray[i].content;
-        document.getElementById(msg_wall).innerHTML += "<textarea readonly class='message_on_wall'>" + writer +
+    for (i = 0; i < messages.length; i++) {
+        var content = messages[i].content;
+        var writer = messages[i].writer;
+
+        document.getElementById(msg_wall).innerHTML += "<textarea readonly class='posted_message'>" + writer +
             ": " + content + "</textarea>";
     }
 };
 
 
-/* Browse panel */
-
-/* Retrieves profile information about the user and its message wall. */
-searchForUser = function (event) {
+browseUser = function (event) {
     event.preventDefault();
     document.getElementById('user_not_found').innerText = "";
     var token = localStorage.getItem('token');
     var browse_email = document.getElementById('search_email').value;
-    var returnObject = serverstub.getUserDataByEmail(token, browse_email);
+    var serverResponse = serverstub.getUserDataByEmail(token, browse_email);
 
-    if (returnObject.success) {
-        var name = returnObject.data.firstname;
-        var family_name = returnObject.data.familyname;
-        var email = returnObject.data.email;
-        var gender = returnObject.data.gender;
-        var city = returnObject.data.city;
-        var country = returnObject.data.country;
+    if (serverResponse.success) {
+        var name = serverResponse.data.firstname;
+        var family_name = serverResponse.data.familyname;
+        var email = serverResponse.data.email;
+        var gender = serverResponse.data.gender;
+        var city = serverResponse.data.city;
+        var country = serverResponse.data.country;
         document.getElementById('user_name').innerText = name + " " + family_name;
         document.getElementById('user_email').innerText = email;
         document.getElementById('user_gender').innerText = gender;
@@ -168,13 +166,12 @@ searchForUser = function (event) {
         document.getElementById('user_profile').style.display = "block";
     } else {
         document.getElementById('user_profile').style.display = "none";
-        document.getElementById('user_not_found').innerText = returnObject.message;
+        document.getElementById('user_not_found').innerText = serverResponse.message;
     }
 };
 
-/* Account panel */
 
-sign_out = function (event) {
+logOut = function (event) {
     event.preventDefault();
     serverstub.signOut(localStorage.getItem("token"));
     localStorage.removeItem("token");
@@ -182,13 +179,13 @@ sign_out = function (event) {
 };
 
 
-new_password = function (event) {
+changePassword = function (event) {
     event.preventDefault();
     var old_password = document.getElementById("old_password").value;
     var new_password = document.getElementById("new_password").value;
-    var returnObject = serverstub.changePassword(localStorage.getItem("token"), old_password, new_password);
-    document.getElementById("new_pwd_feedback").innerHTML = returnObject.message;
-    if (!returnObject.success) {
+    var serverResponse = serverstub.changePassword(localStorage.getItem("token"), old_password, new_password);
+    document.getElementById("new_pwd_feedback").innerHTML = serverResponse.message;
+    if (!serverResponse.success) {
     } else {
         document.getElementById('old_password').value = '';
         document.getElementById('new_password').value = '';
@@ -196,24 +193,23 @@ new_password = function (event) {
     }
 };
 
-/* Login functionality */
+
 logIn = function (event) {
     event.preventDefault();
     var login_email = document.getElementById('login_email');
     var login_password = document.getElementById('login_password');
-    var returnObject = serverstub.signIn(login_email.value, login_password.value);
-    if (!returnObject.success) {
-        document.getElementById("login_error").innerHTML = returnObject.message;
+    var serverResponse = serverstub.signIn(login_email.value, login_password.value);
+    if (!serverResponse.success) {
+        document.getElementById("login_error").innerHTML = serverResponse.message;
     } else {
         document.getElementById("login_error").innerHTML = '';
-        localStorage.setItem('token', returnObject.data);
+        localStorage.setItem('token', serverResponse.data);
         displayView();
     }
 };
 
 
-/* Validates that two passwords match. */
-pwdMatch = function (first_password_id, second_password_id) {
+passwordMatch = function (first_password_id, second_password_id) {
     var password1 = document.getElementById(first_password_id);
     var password2 = document.getElementById(second_password_id);
     if (password1.value !== password2.value) {
@@ -223,8 +219,8 @@ pwdMatch = function (first_password_id, second_password_id) {
     }
 };
 
-/* Validates that the password is long enough */
-pwdLength = function (password_id) {
+
+passwordLength = function (password_id) {
     var password1 = document.getElementById(password_id).value;
     if (password1.length < 8) {
         document.getElementById(password_id).setCustomValidity('Password need to be at least 8 characters.');
@@ -233,12 +229,11 @@ pwdLength = function (password_id) {
     }
 };
 
-/* Resets the custom validity if email was already registered. */
 resetValidation = function () {
     document.getElementById('signup_email').setCustomValidity('');
 };
 
-/* Tries to register a user. If it works it also logs in the user. */
+
 registerUser = function (event) {
     event.preventDefault();
     var signup_form = document.getElementById("signup_form");
