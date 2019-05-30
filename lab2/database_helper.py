1cfabe117c1e2b8560ac8bed125ef43ac21c34a1
johmy592 <johmy592@student.liu.se>
2019-05-30 23:58:24 +0200

    Updated lab2

--- lab2/database_helper.py
+++ lab2/database_helper.py
@@ -3,36 +3,32 @@ import sqlite3
 connection = sqlite3.connect('database.db')
 c = connection.cursor()
 
-"""
-This is where all the code handeling the database should be.
-"""
+
+def sign_in_user(email, token):
+    c.execute("SELECT * FROM signedInUsers WHERE email = ?", (email,))
+    if c.fetchone():
+        c.execute("UPDATE signedInUsers SET token = ? WHERE email = ?", (token, email))
+    else:
+        c.execute("INSERT INTO signedInUsers(email, token) VALUES (?, ?)", (email, token))
+    connection.commit()
 
 
-def add_user_to_db(email, password, firstname, familyname, gender, city, country):
-    c.execute("""INSERT INTO users(email, password, firstname, familyname, gender, city, country) VALUES 
+def add_new_user(email, password, firstname, familyname, gender, city, country):
+    c.execute("""INSERT INTO users(email, password, firstname, familyname, gender, city, country) VALUES
             (?, ?, ?, ?, ?, ?, ?)""", (email, password, firstname, familyname, gender, city, country))
     connection.commit()
 
 
-def sign_in_user(email, token):
-    c.execute("SELECT * FROM loggedInUsers WHERE email = ?", (email,))
-    if c.fetchall():
-        c.execute("UPDATE loggedInUsers SET token = ? WHERE email = ?", (token, email))
-    else:
-        c.execute("INSERT INTO loggedInUsers(email, token) VALUES (?, ?)", (email, token))
-    connection.commit()
-
-
 def sign_out_user(token):
-    c.execute('DELETE FROM loggedInUsers WHERE token = ?', (token,))
+    c.execute('DELETE FROM signedInUsers WHERE token = ?', (token,))
     connection.commit()
 
 
-def token_to_email(token):
-    c.execute('SELECT email FROM loggedInUsers WHERE token = ?', (token,))
-    result = c.fetchall()
+def email_from_token(token):
+    c.execute('SELECT email FROM signedInUsers WHERE token = ?', (token,))
+    result = c.fetchone()
     if result:
-        email = result[0][0]
+        email = result[0]
         return email
     return ""
 
@@ -40,41 +36,31 @@ def token_to_email(token):
 def get_messages_from_db(email):
     c.execute('SELECT sender, message FROM messages WHERE reciever=?', (email,))
     result = c.fetchall()
-    i = 0
     data = []
-    while i < len(result):
+    for i in range(len(result)):
         writer = result[i][0]
         content = result[i][1]
         message_dict = {"writer": writer, "content": content}
         data.append(message_dict)
-        i = i + 1
     return data
 
 
-def user_exists(email):
-    user_data_list = get_user_data_from_db(email)
-    if user_data_list:
+def find_user(email):
+    user_data = get_user_data_from_db(email)
+    if user_data:
         return True
     return False
 
 
 def get_user_data_from_db(email):
     c.execute('SELECT * FROM users WHERE email = ?', (email,))
-    user_data_list = c.fetchall()
-    return user_data_list
+    user_data = c.fetchone()
+    return user_data
 
 
-def add_message_to_db(receiver, sender, message):
-    c.execute('INSERT INTO messages(reciever, sender, message) VALUES (?, ?, ?)', (receiver, sender, message))
+def create_post(recipient, sender, message):
+    c.execute('INSERT INTO messages(reciever, sender, message) VALUES (?, ?, ?)', (recipient, sender, message))
     connection.commit()
-
-
-def user_exists(email):
-    c.execute('SELECT * FROM users WHERE email = ?', (email,))
-    user_data_list = c.fetchall()
-    if user_data_list:
-        return True
-    return False
 
 
 def update_password(email, new_password):
@@ -82,15 +68,15 @@ def update_password(email, new_password):
     connection.commit()
 
 
-def user_and_password_match(email, password):
+def email_and_password_match(email, password):
     c.execute('SELECT * FROM users WHERE email = ? AND password = ?', (email, password))
-    if c.fetchall():
+    if c.fetchone():
         return True
     return False
 
 
-def user_logged_in(token):
-    email = token_to_email(token)
+def user_signed_in(token):
+    email = email_from_token(token)
     if email:
         return True
     return False
--- lab2/test_helper.py
+++ lab2/test_helper.py
@@ -1,11 +1,14 @@
 import requests
 
-"""
-Helper functions for test cases in test.py
-"""
+
+def test_sign_in(email, password):
+    url = 'http://127.0.0.1:5000/sign_in'
+    login_data = {"email": email, "password": password}
+    response = requests.post(url, json=login_data)
+    return response
 
 
-def http_signup(email, password, firstname, familyname, gender, city, country):
+def test_sign_up(email, password, firstname, familyname, gender, city, country):
     url = 'http://127.0.0.1:5000/sign_up'
     signup_data = {"email": email, "password": password, "firstname": firstname, "familyname": familyname,
                    "gender": gender, "city": city, "country": country}
@@ -13,57 +16,50 @@ def http_signup(email, password, firstname, familyname, gender, city, country):
     return response
 
 
-def http_sign_out(token):
+def test_sign_out(token):
     url = 'http://127.0.0.1:5000/sign_out'
     login_data = {"token": token}
     response = requests.post(url, json=login_data)
     return response
 
 
-def http_sign_in(email, password):
-    url = 'http://127.0.0.1:5000/sign_in'
-    login_data = {"email": email, "password": password}
-    response = requests.post(url, json=login_data)
-    return response
-
-
-def http_change_pwd(token, oldPassword, newPassword):
+def test_change_pwd(token, old_password, new_password):
     url = "http://127.0.0.1:5000/account/change_password"
-    data = {"token": token, "oldPassword": oldPassword, "newPassword": newPassword}
+    data = {"token": token, "oldPassword": old_password, "newPassword": new_password}
     response = requests.post(url, json=data)
     return response
 
 
-def http_user_data_by_token(token):
+def test_user_data_by_token(token):
     url = "http://127.0.0.1:5000/home/get_user_data_by_token"
     data = {"token": token}
-    response = requests.get(url, params=data)
+    response = requests.get(url, json=data)
     return response
 
 
-def http_user_data_by_email(token, email):
+def test_user_data_by_email(token, email):
     url = "http://127.0.0.1:5000/browse/get_user_data_by_email"
     data = {"token": token, "email": email}
-    response = requests.get(url, params=data)
+    response = requests.get(url, json=data)
     return response
 
 
-def http_user_messages_by_token(token):
+def test_user_messages_by_token(token):
     url = "http://127.0.0.1:5000/home/get_user_messages_by_token"
     data = {"token": token}
-    response = requests.get(url, params=data)
+    response = requests.get(url, json=data)
     return response
 
 
-def http_user_messages_by_email(token, email):
+def test_user_messages_by_email(token, email):
     url = "http://127.0.0.1:5000/browse/get_user_messages_by_email"
     data = {"token": token, "email": email}
-    response = requests.get(url, params=data)
+    response = requests.get(url, json=data)
     return response
 
 
-def http_post_message(token, email, msg):
+def test_post_message(token, email, message):
     url = "http://127.0.0.1:5000/post_message"
-    data = {"token": token, "email": email, "message": msg}
+    data = {"token": token, "email": email, "message": message}
     response = requests.post(url, json=data)
     return response
